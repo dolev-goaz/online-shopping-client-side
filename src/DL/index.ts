7f59fbe332d3e06110dedeed7808532c8c3b942e
Dolev <www.dolev@gmail.com>
2023-08-09 21:46:02 +0300

    local fake api

--- src/DL/index.ts
+++ src/DL/index.ts
@@ -20,13 +20,22 @@ export async function SignIn(username: string, password: string): Promise<User |
 
 export async function getProducts(): Promise<Product[]> {
     // test items
-    const items: any[] = await axiosInstance.get('https://fakestoreapi.com/products').then((res) => res.data);
-    return items.map((item) => ({
-        Price: item.price,
-        ProductName: item.title,
-        ProductId: item.id,
-        ProductDesc: item.description,
-        Stock: Math.floor(Math.random() * 100),
-        Image: item.image
-    } as unknown as Product));
+    // const items: any[] = await axiosInstance.get('https://fakestoreapi.com/products').then((res) => res.data);
+    // return items.map((item) => ({
+    //     Price: item.price,
+    //     ProductName: item.title,
+    //     ProductId: item.id,
+    //     ProductDesc: item.description,
+    //     Stock: Math.floor(Math.random() * 100),
+    //     Image: item.image
+    // } as unknown as Product));
+
+    return Array.from({length: 50}).map((_, ind) => ({
+        Image: `https://picsum.photos/id/${ind}/500/700`,
+        Price: parseFloat((Math.random() * 100).toFixed(2)),
+        Stock: Math.floor(Math.random() * 15),
+        ProductId: ind.toString(),
+        ProductName: "מוצר רנדומלי",
+        ProductDesc: "תיאור מוצר מגניב רצח".repeat(1 + Math.floor(Math.random() * 5))
+    }));
 }
