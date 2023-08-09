f9c0a00addefeea9930737db9bcee4ee7c48046a
Dolev <www.dolev@gmail.com>
2023-08-09 22:13:29 +0300

    responsive

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,14 +1,12 @@
 <template>
     <LoadWrapper :loading="productStore.loadingProducts">
-        <div class="products-page">
-            <ul class="products-container">
-                <li v-for="product in productStore.products" :key="product.ProductId">
-                    <a :href="`#/product/${product.ProductId}`">
-                        <Product :product="product" />
-                    </a>
-                </li>
-            </ul>
-        </div>
+        <ul class="products-container">
+            <li v-for="product in productStore.products" :key="product.ProductId">
+                <a :href="`#/product/${product.ProductId}`">
+                    <Product :product="product" />
+                </a>
+            </li>
+        </ul>
     </LoadWrapper>
 </template>
 <script lang="ts" setup>
@@ -22,16 +20,24 @@ productStore.getProducts();
 
 </script>
 <style lang="scss" scoped>
-.products-page {
-    padding-inline: 20rem;
-}
-
 .products-container {
+    --items-per-row: 4;
     margin-top: 1rem;
     display: grid;
     gap: 1rem;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(var(--items-per-row), 1fr);
     padding: 1rem;
+    width: min(90%, 100rem);
+    margin-inline: auto;
+}
+
+@for $i from 1 through 3 {
+    @media (max-width: calc((4 - $i) * 35rem)) {
+        .products-container {
+            --items-per-row: calc(4 - #{$i});
+        }
+    }
+
 }
 
 ul {
