11909ee080e1e7441819ce7ed887025268336ca7
Dolev <www.dolev@gmail.com>
2023-08-24 19:46:12 +0300

    upload icon on image

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -3,9 +3,13 @@
         <div class="product-page" v-if="editedProduct">
             <div class="product-content">
                 <div class="image-wrapper">
-                    <input v-if="authStore.isAdmin" type="file" accept="image/*" id="product-image" @change="onSetImage" />
+                    <template v-if="authStore.isAdmin">
+                        <VIcon class="upload-image-icon">mdi-upload</VIcon>
+                        <input type="file" accept="image/*" id="product-image" @change="onSetImage" />
+                    </template>
                     <label for="product-image">
-                        <ImagePreview class="image" :src="editedProduct.imagePath ?? fallbackImage" :alt="editedProduct.title" />
+                        <ImagePreview :is-active="!authStore.isAdmin" class="image" :src="editedProduct.imagePath ?? fallbackImage"
+                            :alt="editedProduct.title" />
                     </label>
                 </div>
                 <div class="data">
@@ -37,8 +41,8 @@
                         <span>{{ t('currency', { value: price }) }}</span>
                     </div>
                     <div class="actions">
-                        <MyButton :loading="loadingSave" v-if="authStore.isAdmin" :disabled="!wasProductChanged || loadingSave"
-                            @click="onSaveChanges">
+                        <MyButton :loading="loadingSave" v-if="authStore.isAdmin"
+                            :disabled="!wasProductChanged || loadingSave" @click="onSaveChanges">
                             {{ t('actions.saveChanges') }}
                         </MyButton>
                         <MyButton v-else :disabled="!currentProduct || !currentProduct.stock" @click="onPurchase">
@@ -168,7 +172,7 @@ const wasProductChanged = computed(() => {
 const uploadImage = ref<File>();
 
 function onSetImage(event: Event) {
-    const target =<HTMLInputElement>event.target;
+    const target = <HTMLInputElement>event.target;
     const files = [...target.files!]
     if (files.length == 0) return;
     uploadImage.value = files[0];
@@ -257,15 +261,48 @@ async function onSaveChanges() {
 
 
 .image-wrapper {
+    height: max-content;
+    position: relative;
     justify-self: center;
     display: flex;
     flex-direction: column-reverse;
     justify-content: start;
+
     input[type=file] {
         width: 0;
+        height: 0;
     }
-    input[type=file] + label {
+
+    input[type=file]+label {
         cursor: pointer;
+    }
+
+    &:has(input[type=file]) .image {
+        filter: brightness(0.7) blur(1px);
+    }
+    .upload-image-icon {
+        position: absolute;
+        z-index: 2;
+        top: 50%;
+        left: 50%;
+        translate: -50% -50%;
+        
+        font-size: 5rem;
+        color: white;
+        border: 1px solid white;
+        border-radius: 50%;
+
+        width: 5rem;
+        height: 5rem;
+        padding: 5rem;
+        background-color: rgba(white, 0.5);
+        pointer-events: none;
+
+        transition: scale 100ms ease;
+    }
+
+    &:hover .upload-image-icon {
+        scale: 1.3;
     }
 }
 
