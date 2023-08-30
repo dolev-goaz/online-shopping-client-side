a5d78234551a1fda71058176c76839003617b76a
Dolev <www.dolev@gmail.com>
2023-08-30 23:34:41 +0300

    password min length

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,7 @@
                 <TextInput type="email" v-model="mail" name="mail" required :label="t('form.login.mail')"
                     :placeholder="t('form.login.mail')" />
                 <TextInput type="password" v-model="password" name="password" required :label="t('form.login.password')"
-                    :placeholder="t('form.login.password')" />
+                    :placeholder="t('form.login.password')" minlength="4" />
                 <template #submit-button>{{ t('authentication.login') }}</template>
             </BaseForm>
             <RouterLink to="/register">
